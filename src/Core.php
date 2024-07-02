98dfbc6d48a75dd4d1fde32ce0cbc1c08bd84cc1
RudyMasGoNext <rudy.mas@go-next.be>
2024-07-02 11:56:36 +0200

    Added tigress/database code to core

--- src/Core.php
+++ src/Core.php
@@ -2,6 +2,7 @@
 
 namespace Tigress;
 
+use Exception;
 use stdClass;
 use Twig\Error\LoaderError;
 
@@ -11,22 +12,24 @@ use Twig\Error\LoaderError;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 0.0.6
+ * @version 0.1.0
  * @package Tigress
  */
 class Core
 {
     public TwigHelper $Twig;
+    public Database $Database;
     public stdClass $Config;
     public stdClass $Routes;
     public stdClass $System;
 
     /**
      * @throws LoaderError
+     * @throws Exception
      */
     public function __construct()
     {
-        define('TIGRESS_CORE_VERSION', '0.0.6');
+        define('TIGRESS_CORE_VERSION', '0.1.0');
 
         $this->settingUpRootMapping();
 
@@ -42,10 +45,52 @@ class Core
         $this->Routes = json_decode(file_get_contents('config/routes.json'));
         $this->System = json_decode(file_get_contents('system/config.json'));
 
+        if ($this->Config->packages->tigress_database === true) {
+            if(!$this->connectDatabase()) throw new Exception('No database connection possible', 500);
+        }
+
         $this->Twig = new TwigHelper($this->System->Core->Twig->views, $this->System->Core->Twig->debug);
         $this->Twig->addPath('vendor/tigress/core/src/views');
 
         $Router = new Router($this);
+    }
+
+    /**
+     * Connect to the database
+     *
+     * @return bool
+     * @throws Exception
+     */
+    private function connectDatabase(): bool
+    {
+        foreach ($this->Config->servers as $server => $type) {
+            if (isset($_SERVER['HTTP_HOST'])) {
+                if (strpos($_SERVER['HTTP_HOST'], $server)) {
+                    $this->Database = new Database(
+                        $this->Config->databases->$type->host,
+                        $this->Config->databases->$type->port,
+                        $this->Config->databases->$type->username,
+                        $this->Config->databases->$type->password,
+                        $this->Config->databases->$type->database,
+                        $this->Config->databases->$type->charset,
+                        $this->Config->databases->$type->dbtype,
+                    );
+                    return true;
+                }
+            } else {
+                $this->Database = new Database(
+                    $this->Config->databases->development->host,
+                    $this->Config->databases->development->port,
+                    $this->Config->databases->development->username,
+                    $this->Config->databases->development->password,
+                    $this->Config->databases->development->database,
+                    $this->Config->databases->development->charset,
+                    $this->Config->databases->development->dbtype,
+                );
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
