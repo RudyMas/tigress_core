c1dea9a2f94604092dce71be623f71301e44a904
RudyMasGoNext <rudy.mas@go-next.be>
2024-11-27 09:04:31 +0100

    Layout aanpassing

--- src/Core.php
+++ src/Core.php
@@ -26,8 +26,8 @@ use Twig\Error\LoaderError;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 0.9.2
- * @lastmodified 2024-11-15
+ * @version 0.9.3
+ * @lastmodified 2024-11-27
  * @package Tigress\Core
  */
 class Core
@@ -43,7 +43,7 @@ class Core
      */
     public function __construct()
     {
-        define('TIGRESS_CORE_VERSION', '0.9.2');
+        define('TIGRESS_CORE_VERSION', '0.9.3');
 
         // Create BASE_URL, SYSTEM_ROOT & others
         $this->settingUpRootMapping();
