5e7f60c77a46372514b7a7d7c78ea8b94182ff34
RudyMasGoNext <rudy.mas@go-next.be>
2025-03-28 12:09:57 +0100

    Added multilanguage to PdfCreatorHelper

--- src/PdfCreatorHelper.php
+++ src/PdfCreatorHelper.php
@@ -11,12 +11,13 @@ use Dompdf\Dompdf;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.01.21.1
+ * @version 2025.03.28.0
  * @package Tigress\PdfCreatorHelper
  */
 class PdfCreatorHelper
 {
     private Dompdf $Dompdf;
+    private string $language = 'nl';
 
     /**
      * Get the version of the PdfCreatorHelper
@@ -25,7 +26,7 @@ class PdfCreatorHelper
      */
     public static function version(): string
     {
-        return '2025.01.21';
+        return '2025.03.28';
     }
 
     /**
@@ -44,7 +45,7 @@ class PdfCreatorHelper
      * @param string $orientation
      * @param string $filename
      * @param string $filepath
-     * @param bool $paginatie
+     * @param bool $pagination
      * @param int $attachment
      * @return void
      */
@@ -54,7 +55,7 @@ class PdfCreatorHelper
         string $orientation = 'portrait',
         string $filename = 'document.pdf',
         string $filepath = '/public/tmp/',
-        bool   $paginatie = false,
+        bool   $pagination = false,
         int    $attachment = 1
     ): void
     {
@@ -62,7 +63,7 @@ class PdfCreatorHelper
         $this->Dompdf->setPaper($format, $orientation);
         $this->Dompdf->render();
 
-        if ($paginatie) {
+        if ($pagination) {
             $canvas = $this->Dompdf->getCanvas();
             $page = [
                 'A3' => [
@@ -79,10 +80,12 @@ class PdfCreatorHelper
                 ],
             ];
 
+            $paginationText = $this->getPaginationText();
+
             $canvas->page_text(
                 $page[$format][$orientation]['x'],
                 $page[$format][$orientation]['y'],
-                "Pagina {PAGE_NUM} van {PAGE_COUNT}",
+                $paginationText,
                 null,
                 8
             );
@@ -134,5 +137,70 @@ class PdfCreatorHelper
                 return "<img src='$base64' alt='$alt'/>";
             }
         }
+    }
+
+    /**
+     * Set the language of the PDF file.
+     *
+     * @param string $language
+     * @return void
+     */
+    public function setLanguage(string $language): void
+    {
+        $this->language = $language;
+    }
+
+    private function getPaginationText(): string
+    {
+        return match ($this->language) {
+            'ar' => "الصفحة {PAGE_NUM} من {PAGE_COUNT}",
+            'az' => "Səhifə {PAGE_NUM} / {PAGE_COUNT}",
+            'be' => "Старонка {PAGE_NUM} з {PAGE_COUNT}",
+            'bg' => "Страница {PAGE_NUM} от {PAGE_COUNT}",
+            'bs' => "Stranica {PAGE_NUM} od {PAGE_COUNT}",
+            'cs' => "Strana {PAGE_NUM} z {PAGE_COUNT}",
+            'da' => "Side {PAGE_NUM} af {PAGE_COUNT}",
+            'de' => "Seite {PAGE_NUM} von {PAGE_COUNT}",
+            'el' => "Σελίδα {PAGE_NUM} από {PAGE_COUNT}",
+            'en' => "Page {PAGE_NUM} of {PAGE_COUNT}",
+            'es', 'pt' => "Página {PAGE_NUM} de {PAGE_COUNT}",
+            'et' => "Lehekülg {PAGE_NUM} / {PAGE_COUNT}",
+            'fi' => "Sivu {PAGE_NUM} / {PAGE_COUNT}",
+            'fr' => "Page {PAGE_NUM} sur {PAGE_COUNT}",
+            'hi' => "पृष्ठ {PAGE_NUM} का {PAGE_COUNT} में",
+            'hr' => "Stranica {PAGE_NUM} od {PAGE_COUNT}",
+            'hu' => "Oldal {PAGE_NUM} / {PAGE_COUNT}",
+            'hy' => "Էջ {PAGE_NUM} է {PAGE_COUNT}-ից",
+            'id' => "Halaman {PAGE_NUM} dari {PAGE_COUNT}",
+            'it' => "Pagina {PAGE_NUM} di {PAGE_COUNT}",
+            'ja' => "ページ {PAGE_NUM} / {PAGE_COUNT}",
+            'ka' => "გვერდი {PAGE_NUM} სულ {PAGE_COUNT}-დან",
+            'kk' => "Бет {PAGE_NUM} / {PAGE_COUNT}",
+            'ko' => "페이지 {PAGE_NUM} / {PAGE_COUNT}",
+            'ky' => "Бет {PAGE_NUM} / {PAGE_COUNT}",
+            'lt' => "Puslapis {PAGE_NUM} iš {PAGE_COUNT}",
+            'lv' => "Lapa {PAGE_NUM} no {PAGE_COUNT}",
+            'mk' => "Страна {PAGE_NUM} од {PAGE_COUNT}",
+            'mn' => "Хуудас {PAGE_NUM} / {PAGE_COUNT}",
+            'ms' => "Halaman {PAGE_NUM} daripada {PAGE_COUNT}",
+            'nl' => "Pagina {PAGE_NUM} van {PAGE_COUNT}",
+            'no' => "Side {PAGE_NUM} av {PAGE_COUNT}",
+            'pl' => "Strona {PAGE_NUM} z {PAGE_COUNT}",
+            'ro' => "Pagina {PAGE_NUM} din {PAGE_COUNT}",
+            'ru' => "Страница {PAGE_NUM} из {PAGE_COUNT}",
+            'sk' => "Strana {PAGE_NUM} z {PAGE_COUNT}",
+            'sl' => "Stran {PAGE_NUM} od {PAGE_COUNT}",
+            'sq' => "Faqja {PAGE_NUM} nga {PAGE_COUNT}",
+            'sr' => "Страница {PAGE_NUM} од {PAGE_COUNT}",
+            'sv' => "Sida {PAGE_NUM} av {PAGE_COUNT}",
+            'th' => "หน้า {PAGE_NUM} จาก {PAGE_COUNT}",
+            'tg' => "Саҳифа {PAGE_NUM} аз {PAGE_COUNT}",
+            'tr' => "Sayfa {PAGE_NUM} / {PAGE_COUNT}",
+            'uk' => "Сторінка {PAGE_NUM} з {PAGE_COUNT}",
+            'uz' => "Sahifa {PAGE_NUM} {PAGE_COUNT} dan",
+            'vi' => "Trang {PAGE_NUM} của {PAGE_COUNT}",
+            'zh' => "第 {PAGE_NUM} 页，共 {PAGE_COUNT} 页",
+            default => "Page {PAGE_NUM} of {PAGE_COUNT}",
+        };
     }
 }
