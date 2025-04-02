c69b9e2dec3a9a710bd754cd0d7ea7e546acd595
RudyMasGoNext <rudy.mas@go-next.be>
2025-04-02 11:49:25 +0200

    Added html class 'auto-grow'

--- public/javascript/tigress.js
+++ public/javascript/tigress.js
@@ -1,6 +1,19 @@
+function autoResize($el) {
+    $el.css('height', 'auto');
+    $el.css('height', $el.prop('scrollHeight') + 'px');
+}
+
 $(function () {
     $('select').select2({
         theme: 'bootstrap-5',
         placeholder: $(this).data('placeholder'),
+    });
+
+    $('.auto-grow').each(function () {
+        autoResize($(this));
+    });
+
+    $(document).on('input', '.auto-grow', function () {
+        autoResize($(this));
     });
 });
