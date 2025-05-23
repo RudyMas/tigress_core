e712582ec2f15befbd043bd3431f21e98938a8b8
RudyMasGoNext <rudy.mas@go-next.be>
2025-05-23 15:41:22 +0200

    Automatically committed by the system on 2025-05-23

--- src/controllers/version/VersionController.php
+++ src/controllers/version/VersionController.php
@@ -13,6 +13,7 @@ use Tigress\DataConverter;
 use Tigress\DataFiles;
 use Tigress\DisplayHelper;
 use Tigress\Encryption;
+use Tigress\FormBuilder;
 use Tigress\FileManager;
 use Tigress\FrameworkHelper;
 use Tigress\GoogleApi;
@@ -81,6 +82,7 @@ class VersionController
         $tigress_data_converter_version = class_exists('Tigress\DataConverter') ? DataConverter::version() : 'Not Active';
         $tigress_data_files_version = class_exists('Tigress\DataFiles') ? DataFiles::version() : 'Not Active';
         $tigress_file_manager_version = class_exists('Tigress\FileManager') ? FileManager::version() : 'Not Active';
+        $tigress_form_builder_version = class_exists('Tigress\FormBuilder') ? FormBuilder::version() : 'Not Active';
         $tigress_http_requests_version = class_exists('Tigress\HttpRequests') ? HttpRequests::version() : 'Not Active';
         $tigress_communication_version = class_exists('Tigress\Communication') ? Communication::version() : ['Communication' => 'Not Active'];
         $tigress_google_api_version = class_exists('Tigress\GoogleApi') ? GoogleApi::version() : ['GoogleApi' => 'Not Active'];
@@ -106,6 +108,7 @@ class VersionController
             'tigress_data_converter_version' => $tigress_data_converter_version,
             'tigress_data_files_version' => $tigress_data_files_version,
             'tigress_file_manager_version' => $tigress_file_manager_version,
+            'tigress_form_builder_version' => $tigress_form_builder_version,
             'tigress_http_requests_version' => $tigress_http_requests_version,
             'tigress_communication_version' => $tigress_communication_version,
             'tigress_google_api_version' => $tigress_google_api_version,
