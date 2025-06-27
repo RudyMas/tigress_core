c934b98d01edcb47ab1ed01cdb16c9677bb18c3f
RudyMasGoNext <rudy.mas@go-next.be>
2025-06-27 11:42:58 +0200

    Update __() translation function

--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -4,13 +4,14 @@
  * Translate a word/sentence to the current language
  *
  * @param string $word
+ * @param string $translationFile
  * @return string
  */
-function __(string $word): string
+function __(string $word, string $translationFile = SYSTEM_ROOT . '/translations/translations.json'): string
 {
     static $translations = null;
     if ($translations === null) {
-        $translations = json_decode(file_get_contents(SYSTEM_ROOT . '/translations/translations.json'), true);
+        $translations = json_decode(file_get_contents($translationFile), true);
     }
 
     $lang = substr(CONFIG->website->html_lang, 0, 2);
