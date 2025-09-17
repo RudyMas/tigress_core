08f457bd37638796c3f5a2433b6d12672f16c996
RudyMasGoNext <rudy.mas@go-next.be>
2025-09-17 10:08:15 +0200

    Changed TWIG function in_value so an array of json can be given as the haystack

--- src/DisplayHelper.php
+++ src/DisplayHelper.php
@@ -24,7 +24,7 @@ use Twig\TwigFunction;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.08.27.1
+ * @version 2025.09.17.0
  * @package Tigress\DisplayHelper
  */
 class DisplayHelper
@@ -39,7 +39,7 @@ class DisplayHelper
      */
     public static function version(): string
     {
-        return '2025.08.27';
+        return '2025.09.17';
     }
 
     /**
@@ -123,7 +123,11 @@ class DisplayHelper
         }));
 
         $this->twig->addFunction(new TwigFunction('in_values', function ($needle, $haystack, $strict = false): bool {
-            return in_array($needle, array_values($haystack), $strict);
+            if (is_array($haystack)) {
+                return in_array($needle, array_values($haystack), $strict);
+            } else {
+                return in_array($needle, array_values(json_decode($haystack)), $strict);
+            }
         }));
 
         $this->twig->addFunction(new TwigFunction('match', function (string $pattern, string $subject): array {
