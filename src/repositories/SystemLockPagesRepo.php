ff496f7d7ef989fe341b51024d18c2b00d4780d1
RudyMasGoNext <rudy.mas@go-next.be>
2026-01-09 09:06:09 +0100

    Added automatic creation of a table by ->createTable in the Repository of SystemLockPagesRepo

--- src/repositories/SystemLockPagesRepo.php
+++ src/repositories/SystemLockPagesRepo.php
@@ -16,6 +16,17 @@ class SystemLockPagesRepo extends Repository
         $this->primaryKey = ['resource', 'resource_id'];
         $this->model = 'DefaultModel';
         $this->autoload = true;
+        $this->createTable = [
+            "CREATE TABLE `system_lock_pages` (
+              `resource` varchar(50) NOT NULL,
+              `resource_id` int(11) NOT NULL,
+              `locked_by_user_id` int(11) NOT NULL,
+              `locked_at` timestamp NOT NULL,
+              `expires_at` timestamp NOT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci",
+            "ALTER TABLE `system_lock_pages`
+              ADD PRIMARY KEY (`resource`,`resource_id`)"
+        ];
         parent::__construct();
     }
 }
