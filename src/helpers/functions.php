c872d8b62a31b7c28abb139a6ba7e2ce5e7fcf61
RudyMasGoNext <rudy.mas@go-next.be>
2026-03-16 12:06:44 +0100

    Added helper function is_json()

--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -11,3 +11,23 @@ function __(string $text): string
     $lang = substr(CONFIG->website->html_lang ?? 'en', 0, 2);
     return TRANSLATIONS->get()[$lang][$text] ?? $text;
 }
+
+/**
+ * Check if the data is JSON data
+ *
+ * @param $string
+ * @return bool
+ */
+function is_json($string): bool
+{
+    if (!is_string($string)) {
+        return false;
+    }
+
+    try {
+        json_decode($string, true, 512, JSON_THROW_ON_ERROR);
+        return true;
+    } catch (JsonException) {
+        return false;
+    }
+}
